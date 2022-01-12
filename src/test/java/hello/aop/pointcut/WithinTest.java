92f02e6ffe83bc4e90d9e006d5fc6d3bc93b4714
chaehwanlim <chlim428@gmail.com>
2022-01-12 23:23:30 +0900

    test: within 테스트를 추가한다

---
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,33 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class WithinTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws Exception {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    @DisplayName("타겟의 타입에만 직접 적용 가능, 인터페이스 불가능")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+}
